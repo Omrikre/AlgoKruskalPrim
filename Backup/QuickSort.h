62aa0dd97e75361e38d0c2a69e92171c680caa62
Omri Krelman <83236913+Omrikre@users.noreply.github.com>
2022-05-14 22:27:15 +0300

    Delete Backup directory

--- Backup/QuickSort.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by Omrik on 5/7/2022.
-//
-
-#ifndef UNTITLED_QUICKSORT_H
-#define UNTITLED_QUICKSORT_H
-
-#endif //UNTITLED_QUICKSORT_H
--- Backup/UnionFind.cpp
+++
@@ -1,7 +0,0 @@
-//
-// Created by Omrik on 5/2/2022.
-//
-
-class UnionFind {
-
-};
